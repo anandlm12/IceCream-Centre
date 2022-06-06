9e8801a77fa5485b92fbc5d0a65f1d697a6d77d0
anandlm12 <anandsingh.mec20.@itbhu.ac.in>
2022-06-06 17:21:38 +0530

    release

---
+++ Hello/Hello/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_header = "Mickey Ice Creams Admin"
+admin.site.site_title = "Mickey Ice Creams Admin Portal"
+admin.site.index_title = "Welcome to Mickey Ice Creams"
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+]
---
+++ Hello/home/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import  Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    context = {
+        'variable1' : "Hey!",
+        'variable2' : "Hey Anand"
+    }
+    messages.success(request, "this is a test message")
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name , email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Profile Details has been successfully sent!')
+    return render(request, 'contact.html')
